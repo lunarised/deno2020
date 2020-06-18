e7cb35e3c46f09b3521259b91158363efacdf947
James McKenzie (1000054567) <lunarised@outlook.com>
2020-06-18 15:32:39 +1200

    99BottlesOfBeerOnTheWallTypeScript

---
+++ 99Bottles.ts
@@ -0,0 +1,20 @@
+let i = 99
+for( i ; i!=2;){
+	console.log(`${i} bottles of beer on the wall`);
+	console.log(`${i} bottles of beer!`);
+	console.log("Take one down and pass it around");
+	i--;
+	console.log(`${i} bottles of beer on the wall\n`);
+
+}
+console.log(`${i} bottles of beer on the wall`);
+console.log(`${i} bottles of beer!`);
+console.log("Take one down and pass it around");
+i--;
+console.log(`${i} bottle of beer on the wall\n`);
+
+console.log(`${i} bottle of beer on the wall`);
+console.log(`${i} bottle of beer!`);
+console.log("Take one down and pass it around");
+i--;
+console.log("No more bottles of beer of the wall")
